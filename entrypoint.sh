0c434a2e09f22d4311d4c19e31687c77df654540
Muhammad Iqbal Alaydrus <iqbal@bangunindo.com>
2023-07-10 13:30:48 +0700

    snmptrapd debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 set -e
 trap2json -generate /etc/trap2json/snmptrapd.conf
 shopt -s lastpipe
-snmptrapd -M +/etc/trap2json/mibs -m ALL -Lo -OnUx -f -C -c /etc/trap2json/snmptrapd.conf | exec trap2json
+if [[ -z "${SNMPTRAPD_DEBUG}" ]]; then
+  snmptrapd -M +/etc/trap2json/mibs -m ALL -Lo -Lf /var/log/trap2json/trap2json.log -OnUx -f -C -c /etc/trap2json/snmptrapd.conf | exec trap2json
+else
+  snmptrapd -M +/etc/trap2json/mibs -m ALL -Lo -OnUx -f -C -c /etc/trap2json/snmptrapd.conf | exec trap2json
+fi
