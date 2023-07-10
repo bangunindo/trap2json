32a8bee250d1416296f956162ee7f4476951ebc5
Muhammad Iqbal Alaydrus <iqbal@bangunindo.com>
2023-07-10 14:10:44 +0700

    fix incorrect env handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 trap2json -generate /etc/trap2json/snmptrapd.conf
 shopt -s lastpipe
 if [[ -z "${SNMPTRAPD_DEBUG}" ]]; then
-  snmptrapd -M +/etc/trap2json/mibs -m ALL -Lo -Lf /var/log/trap2json/trap2json.log -OnUx -f -C -c /etc/trap2json/snmptrapd.conf | exec trap2json
-else
   snmptrapd -M +/etc/trap2json/mibs -m ALL -Lo -OnUx -f -C -c /etc/trap2json/snmptrapd.conf | exec trap2json
+else
+  snmptrapd -M +/etc/trap2json/mibs -m ALL -Lo -Lf /var/log/trap2json/trap2json.log -OnUx -f -C -c /etc/trap2json/snmptrapd.conf | exec trap2json
 fi
