7570238ec7e0487c94790564e216f5b5eaa64685
Muhammad Iqbal Alaydrus <iqbal@bangunindo.com>
2023-10-31 20:38:56 +0700

    implement config defaults via serde

--- src/settings.rs
+++ src/settings.rs
@@ -1,8 +1,12 @@
 use std::fmt::Formatter;
 use serde::Deserialize;
 use serde_enum_str::Deserialize_enum_str;
-use config::{Config, ConfigError, Environment, File};
 use clap::Parser;
+use std::default::Default;
+use crate::rsnmp::{auth, cipher};
+use validator::Validate;
+use std::io::BufReader;
+use std::fs::File;
 
 
 #[derive(Deserialize_enum_str, Clone, Copy, Debug)]
@@ -10,6 +14,12 @@ use clap::Parser;
 pub enum LogFormat {
     Json,
     Console,
+}
+
+impl Default for LogFormat {
+    fn default() -> Self {
+        Self::Console
+    }
 }
 
 #[derive(Deserialize_enum_str, Clone, Copy, Debug)]
@@ -29,6 +39,12 @@ pub enum LogLevel {
     LvlOff,
 }
 
+impl Default for LogLevel {
+    fn default() -> Self {
+        Self::LvlInfo
+    }
+}
+
 impl std::fmt::Display for LogLevel {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -42,22 +58,114 @@ impl std::fmt::Display for LogLevel {
     }
 }
 
-#[derive(Deserialize, Clone, Copy, Debug)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Logger {
+    #[serde(default)]
     pub level: LogLevel,
+    #[serde(default)]
     pub format: LogFormat,
+}
+
+impl Default for Logger {
+    fn default() -> Self {
+        Self {
+            level: Default::default(),
+            format: Default::default(),
+        }
+    }
+}
+
+#[derive(Deserialize, Validate, Clone, Debug)]
+pub struct Community {
+    #[validate(length(min = 1, max = 32))]
+    pub name: String,
+}
+
+#[derive(Deserialize, Validate, Clone, Debug)]
+pub struct User {
+    #[validate(length(min = 1, max = 32))]
+    pub username: String,
+    #[serde(default = "bool::default")]
+    pub no_auth: bool,
+    #[serde(default = "bool::default")]
+    pub require_privacy: bool,
+    #[serde(with = "hex::serde")]
+    pub engine_id: Vec<u8>,
+    pub auth_type: Option<auth::AuthType>,
+    #[validate(length(min = 8))]
+    pub auth_passphrase: Option<String>,
+    pub privacy_protocol: Option<cipher::CipherType>,
+    #[validate(length(min = 8))]
+    pub privacy_passphrase: Option<String>,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct Auth {
+    #[serde(default = "bool::default")]
+    pub enable: bool,
+    #[serde(default = "Vec::new")]
+    pub community: Vec<Community>,
+    #[serde(default = "Vec::new")]
+    pub user: Vec<User>,
+}
+
+impl Default for Auth {
+    fn default() -> Self {
+        Self {
+            enable: false,
+            community: vec![],
+            user: vec![],
+        }
+    }
 }
 
 #[derive(Deserialize, Clone, Debug)]
 pub struct TrapdConfig {
+    #[serde(default = "default_listening")]
     pub listening: Vec<String>,
+    #[serde(default)]
+    pub auth: Auth,
+}
+
+impl Default for TrapdConfig {
+    fn default() -> Self {
+        Self {
+            listening: default_listening(),
+            auth: Default::default(),
+        }
+    }
+}
+
+fn default_listening() -> Vec<String> {
+    vec![
+        "0.0.0.0:10162".to_string(),
+        "[::]:10162".to_string(),
+    ]
 }
 
 #[derive(Deserialize, Clone, Debug)]
 pub struct Settings {
+    #[serde(default)]
     pub logger: Logger,
+    #[serde(default = "default_num_cpus")]
     pub parse_workers: u64,
+    #[serde(default)]
     pub snmptrapd: TrapdConfig,
+}
+
+
+fn default_num_cpus() -> u64 {
+    num_cpus::get() as u64
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            logger: Default::default(),
+            parse_workers: default_num_cpus(),
+            snmptrapd: Default::default(),
+        }
+    }
 }
 
 #[derive(Parser, Debug)]
@@ -68,17 +176,16 @@ struct Args {
 }
 
 impl Settings {
-    pub fn new() -> Result<Self, ConfigError> {
+    pub fn new() -> Result<Self, anyhow::Error> {
         let args = Args::parse();
-        let mut s = Config::builder()
-            .add_source(Environment::with_prefix("T2J"))
-            .set_default("parse_workers", num_cpus::get() as u64)?
-            .set_default("logger.level", "info")?
-            .set_default("logger.format", "console")?
-            .set_default("snmptrapd.listening", vec!["0.0.0.0:10162", "[::]:10162"])?;
-        if let Some(cnf) = args.config {
-            s = s.add_source(File::with_name(cnf.as_str()));
+        match args.config {
+            Some(cnf) => {
+                let file = File::open(cnf)?;
+                let reader = BufReader::new(file);
+                let r: Self = serde_yaml::from_reader(reader)?;
+                Ok(r)
+            },
+            None => Ok(Default::default())
         }
-        s.build()?.try_deserialize()
     }
 }
